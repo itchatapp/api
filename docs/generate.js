7abf06a7c43eb4acc51117067c93d3eee7aab81e
abdulrahman1s <abdulrahman.8alah@gmail.com>
2022-06-04 06:57:37 +0200

    docs: Generate OpenAPI v3 Schema

--- docs/generate.js
+++ docs/generate.js
@@ -1,96 +1,156 @@
 import fs from 'node:fs/promises'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { join } from 'node:path'
+import { readdir, Param, Response, parse } from './utils.js'
 
+const classes = {}
+const enums = {}
+const interfaces = {}
+const types = {}
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-
-async function fetchControllers(dir) {
-  const result = [];
-  const filter = file => ['index.ts', '/Controller.ts', 'decorators.ts'].every(name => !file.endsWith(name))
-
-  async function read(dir) {
-    const files = await fs.readdir(dir)
-
-    for (const filepath of files.map((p) => path.join(dir, p))) {
-      const stat = await fs.stat(filepath)
-
-      if (stat.isDirectory()) {
-        await read(filepath)
-      } else {
-        result.push(filepath)
-      }
+const OpenAPI = {
+    openapi: "3.0.0",
+    info: {
+        title: "ItChatAPI API",
+        version: '1.0.0'
+    },
+    servers: [{
+        url: "https://api.itchat.world",
+        description: "ItChat API"
+    }],
+    paths: {},
+    components: {
+        schemas: {
+            Error: {
+                type: 'object',
+                properties: {
+                    message: { type: 'string' },
+                    status: { type: 'integer' }
+                }
+            }
+        }
     }
-  }
-
-  await read(dir)
-
-  for (let i = 0; i < result.length; i++) {
-    const filePath = result[i]
-
-    if (!filter(filePath)) {
-      delete result[i]
-      continue
-    }
-
-    result[i] = {
-      path: filePath,
-      content: await fs.readFile(filePath, 'utf8')
-    }
-  }
-
-  return result.filter(Boolean)
 }
 
-async function parse(controller) {
+for (const path of await readdir(join(process.cwd(), 'src/structures'))) {
+    if (!['index.ts', '/Base.ts'].every(name => !path.endsWith(name))) continue
 
-  function getPath() {
-    return controller.content.match(/extends Controller\('(.+)'\)/)[1]
-  }
+    const content = await fs.readFile(path, 'utf8')
+
+    for (let [$class, name, extendsThisClass] of content.matchAll(/class\s*([a-z]+)\s*(?:extends ([a-z]+)\s*)?\{(.|\n)*?\n\}/gi)) {
+        if (extendsThisClass && extendsThisClass !== 'Base') name = name + ':' + extendsThisClass
+        classes[name] = $class.replace(/.+\(.*\).*\{(.|\n)*?\n\s*\}/g, '').replace(/static .+\(.+\)(?::\s*.+)/g, '')
+    }
+
+    for (const [$interface, name] of content.matchAll(/interface\s*([a-z]+)\s*\{(.|\n)*?\n\}/gi)) interfaces[name] = $interface
+    for (const [$enum, name] of content.matchAll(/enum\s*([a-z]+)\s*\{(?:.|\n)*?\n\}/gi)) enums[name] = $enum
+    for (const [$type, name] of content.matchAll(/type\s*([a-z]+)\s*=\s*.+/gi)) types[name] = $type
+}
 
 
-  function getRoutes() {
-    const routes = []
+for (const [name, content] of Object.entries(interfaces)) {
+    const schema = OpenAPI.components.schemas[name] ?? (OpenAPI.components.schemas[name] = {})
+
+    for (const [name, type] of parse(content)) {
+        if (type.oneOf) {
+            schema.oneOf = type.oneOf
+        } else if (type.additionalProperties) {
+            schema.additionalProperties = type.additionalProperties
+        } else {
+            if (!schema.required) schema.required = []
+            if (!schema.properties) schema.properties = {}
+            if (!type.nullable) schema.required.push(name)
+            schema.properties[name] = type
+        }
+    }
+}
+
+for (const [$name, content] of Object.entries(classes)) {
+    const [name, extendsThisClass] = $name.split(':')
+    const schema = OpenAPI.components.schemas[name] ?? (OpenAPI.components.schemas[name] = {})
+
+    function process(content) {
+        for (const [name, type] of parse(content)) {
+            if (type.oneOf) {
+                schema.oneOf = type.oneOf
+            } else if (type.additionalProperties) {
+                schema.additionalProperties = type.additionalProperties
+            } else {
+                if (!schema.required) schema.required = []
+                if (!schema.properties) schema.properties = {}
+                if (!type.nullable) schema.required.push(name)
+                schema.properties[name] = type
+            }
+        }
+    }
+
+    if (extendsThisClass) process(classes[extendsThisClass])
+    process(content)
+}
+
+for (const [name, content] of Object.entries(types)) {
+    const [, type] = parse(content)[0] ?? []
+
+    if (!type) {
+        console.log(content)
+        continue
+    }
+
+    OpenAPI.components.schemas[name] = type
+}
+
+
+for (const [name, content] of Object.entries(enums)) {
+    const [, type] = parse(content)[0] ?? []
+
+    if (!type) {
+        console.log(content)
+        continue
+    }
+
+    OpenAPI.components.schemas[name] = type
+}
+
+for (const path of await readdir(join(process.cwd(), 'src/controllers'))) {
+    if (!['index.ts', '/Controller.ts'].every(name => !path.endsWith(name))) continue
+
+    const content = await fs.readFile(path, 'utf8')
+    const basePath = content.match(/path = '(.+)'/)?.[1] ?? ''
     const regex = /'(GET|POST|DELETE|PATCH|PUT) (.+)'\(_?ctx: Context\)(?:: Promise<(.+)> {)?/g
 
     let result;
 
-    while (result = regex.exec(controller.content)) {
-      const [, method, path, type = 'unknown'] = result
-      routes.push({ method, path, type })
+    while (result = regex.exec(content)) {
+        let [, method, path, type = 'unknown'] = result
+
+        method = method.toLowerCase()
+        path = (basePath + path).replace(/:([a-z_]+)/gi, '{$1}')
+
+        if (path.endsWith('/')) path = path.slice(0, -1)
+
+        let schema = OpenAPI.paths[path]?.[method];
+
+        if (!schema) schema = OpenAPI.paths[path] = {
+            [method]: {
+                summary: '',
+                description: '',
+                parameters: [],
+                responses: {},
+            }
+        }[method]
+
+        for (const [, name] of path.matchAll(/{([a-z]+_id)}/gi)) {
+            schema.parameters.push(Param(name))
+        }
+
+        schema.responses.default = Response({ status: 'default' })
+
+        if (type === 'void' || method === 'delete' || type === 'unknown') {
+            schema.responses[204] = Response({ status: 204 })
+        } else {
+            // TODO: Get description
+            schema.responses[200] = Response({ status: 200, type, description: '' })
+        }
     }
-
-    return routes
-  }
-
-
-  controller.path = 'TODO: Change path to docs dir.'
-  controller.content = {
-    basePath: getPath(),
-    routes: getRoutes()
-  }
-
-  console.log(controller)
-
-
-  return controller
 }
 
-async function save(controller) {
-  console.log(controller)
-  //await fs.writeFile(controller.path, JSON.stringify(controller.content, null, 2))
-}
-
-
-fetchControllers(path.join(__dirname, '../src/controllers')).then(controllers => {
-  return Promise.all(controllers.map(parse))
-}).then(controllers => {
-  return Promise.all(controllers.map(save))
-}).then(({ length: size }) => {
-  console.log('Done!')
-  console.log(`Size: ${size}`)
-}).catch(err => {
-  console.error(`Failed generate docs for the following reason: `, err)
-  process.exit(-1)
-})
+await fs.writeFile('docs/OpenAPI.json', JSON.stringify(OpenAPI, null, 2))
---
+++ docs/utils.js
@@ -0,0 +1,143 @@
+import fs from 'node:fs/promises'
+import { join } from 'node:path'
+
+export async function readdir(dir) {
+    const result = [];
+
+    await (async function read(dir) {
+        const files = await fs.readdir(dir)
+        for (const filepath of files.map((p) => join(dir, p))) {
+            const stat = await fs.stat(filepath)
+            if (stat.isDirectory()) await read(filepath)
+            else result.push(filepath)
+        }
+    })(dir);
+
+    return result
+}
+
+export const isPrimitive = (type) => ['string', 'number', 'bigint', 'boolean', 'integer'].includes(type)
+
+
+export function Response({ status, description, type }) {
+    function Json(obj) {
+        return { "application/json": obj }
+    }
+
+    switch (status) {
+        case 204: return { description: "Success" }
+        case 200: {
+            const array = type.endsWith('[]')
+
+            if (array) return {
+                description,
+                content: Json({ schema: Ref(type.slice(0, -2)) })
+            }
+
+            return {
+                description,
+                content: Json({ schema: Ref(type) })
+            }
+        }
+        default: return {
+            description: "An error occurred.",
+            content: Json({ schema: Ref('Error') })
+        }
+    }
+}
+
+
+export function Param(name) {
+    return {
+        name,
+        in: "path",
+        required: true,
+        schema: { type: 'string' }
+    }
+}
+
+export function Primitive(type, nullable = false) {
+    return { type, nullable }
+}
+
+export function Array(type, nullable = false) {
+    return {
+        type: 'array',
+        items: type,
+        nullable
+    }
+}
+
+export function Schema({ type, array, nullable, $enum, record, union }) {
+    if (type === 'number') type = 'integer'
+
+    if (type && isPrimitive(type)) {
+        return array ? Array(Primitive(type, false), nullable) : Primitive(type, nullable)
+    } else if ($enum) {
+        return { type: 'string', enum: $enum }
+    } else if (record) {
+        return { additionalProperties: Schema({ type: record }) }
+    } else if (union) {
+        return { oneOf: [union.map(x => Schema({ type: x }))] }
+    } else {
+        return array ? Array(Ref(type), nullable) : Ref(type)
+    }
+}
+
+
+export function Ref(reference) {
+    return { $ref: `#/components/schemas/${reference}` }
+}
+
+export function parse(content) {
+    const results = []
+    const propertyRegex = /([a-z_?]+)!?: ([a-z\[\]]+)/gi
+    const typeRegex = /type (.+) = (.+)/gi
+    const enumRegex = /enum(.*)\{(?:.|\n)*?\n\}/g
+
+    let result;
+
+    while (result = propertyRegex.exec(content)) {
+        let [, name, type] = result
+
+        if (!name || !type) return null
+
+        const array = type.endsWith('[]')
+        const nullable = name.endsWith('?')
+
+        name = name.replace('?', '')
+        type = type.replace('[]', '')
+
+        results.push([name, Schema({ type, array, nullable, description: '' })])
+    }
+
+    while (result = typeRegex.exec(content)) {
+        let [, name, type] = result
+
+        const [, record] = type.match(/Record<string, ([a-z]+)>/i) ?? []
+
+        if (record) {
+            results.push([name, Schema({ record })])
+            continue
+        }
+
+        const union = type.split(' | ').map(x => x.replace(/'|"|;/g, ''))
+
+        if (union.length > 1) {
+            results.push([name, Schema({ union })])
+            continue
+        }
+
+        results.push([name, Schema({ type })])
+    }
+
+    while (result = enumRegex.exec(content)) {
+        let [, name] = result
+
+        content = content.split('\n').slice(1, -1).join('\n').split(',').map(x => x.replace(/\s|\n/g, ''))
+
+        results.push([name, Schema({ $enum: content })])
+    }
+
+    return results
+}
